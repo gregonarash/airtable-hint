df0152ddd401cba0b8bc205cfc314e92bfff24f4
gregonarash <48462103+gregonarash@users.noreply.github.com>
2023-02-03 21:29:24 +0700

    initial UI components added

--- twind.config.js
+++ twind.config.js
@@ -10,7 +10,7 @@ const presetRemToPx = ({ baseValue = 16 } = {}) => {
         ...rule,
         // d: the CSS declaration body
         // Based on https://github.com/TheDutchCoder/postcss-rem-to-px/blob/main/index.js
-        d: rule.d.replace(
+        d: rule.d?.replace(
           /"[^"]+"|'[^']+'|url\([^)]+\)|(-?\d*\.?\d+)rem/g,
           (match, p1) => {
             if (p1 === undefined) return match;
--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
       "@src": root,
       "@assets": assetsDir,
       "@pages": pagesDir,
+      "@utils": resolve(__dirname, "utils"),
     },
   },
   plugins: [
